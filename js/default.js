af833de576cfdd414e731e24434aee0e6a35dec3
kichy187 <ttynzm1998@gmail.com>
2017-06-19 17:12:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,22 @@
-var number = Math.floor(Math.random()*3);
-var answer=parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
+var number = Math.floor(Math.random()*5);
+var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 
 var message;
-if(answer ===number){
-message='あたり！':
+if(answer === number)
+{
+  message = 'あたり！';
+}
+else if(answer<number)
+{
+  message = '残念、もっと大きいです！';
+}
+else if(answer>number)
+{
+  message = '残念、もっと小さいです！';
+}
+else
+{
+  message = '0~3の数字を入力してください！！';
+}
+
+document.getElementById('choice').textContent = message;
