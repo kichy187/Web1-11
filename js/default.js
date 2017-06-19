9d557ca102ab769e27629993e37918427b4e7fd2
kichy187 <ttynzm1998@gmail.com>
2017-06-19 17:48:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,19 +6,22 @@ while (answer != number){
   if(answer === number)
 {
   message = 'あたり！';
+    document.getElementById('choice').textContent = message;
 }
 else if(answer<number)
 {
   message = '残念、もっと大きいです！';
+    document.getElementById('choice').textContent = message;
 }
 else if(answer>number)
 {
   message = '残念、もっと小さいです！';
+    document.getElementById('choice').textContent = message;
 }
 else
 {
   message = '0~3の数字を入力してください！！';
+    document.getElementById('choice').textContent = message;
 }
-  document.getElementById('choice').textContent = message;
   var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));}
 document.getElementById('choice').textContent = message;
