ce7c396c78a14d9953a65b5bbe28672aa7b4477a
kichy187 <ttynzm1998@gmail.com>
2017-06-19 17:23:35 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -11,14 +11,17 @@ if(answer === number)
 else if(answer<number)
 {
   message = '残念、もっと大きいです！';
+  var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 }
 else if(answer>number)
 {
   message = '残念、もっと小さいです！';
+  var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 }
 else
 {
   message = '0~3の数字を入力してください！！';
+  var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 }
 }
 document.getElementById('choice').textContent = message;
