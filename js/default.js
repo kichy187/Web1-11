3f78f390b653c7ff0ca52a5f581fee1b3b1a3b6f
kichy187 <ttynzm1998@gmail.com>
2017-06-19 17:45:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,8 @@ var number = Math.floor(Math.random()*2);
 var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 var message;
 
-while (x != a)
+while (answer != number)
+  var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 {
   if(answer === number)
 {
@@ -20,6 +21,5 @@ else
 {
   message = '0~3の数字を入力してください！！';
 }
-  var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 }
 document.getElementById('choice').textContent = message;
