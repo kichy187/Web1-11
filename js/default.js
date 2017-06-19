84e2af40fbae10237f895eb501de2627af1fc67e
kichy187 <ttynzm1998@gmail.com>
2017-06-19 17:22:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,12 @@
 var number = Math.floor(Math.random()*5);
 var answer = parseInt(window.prompt('数あてゲーム！０～２の数字を入力してください！！'));
 
+while{
 var message;
 if(answer === number)
 {
   message = 'あたり！';
+  break;
 }
 else if(answer<number)
 {
@@ -18,5 +20,5 @@ else
 {
   message = '0~3の数字を入力してください！！';
 }
-
+}
 document.getElementById('choice').textContent = message;
